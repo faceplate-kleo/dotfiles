27ee379e938fe27a8fd0a2881e2e5112fdb11af8
Kleo Davidson <faceplatemusic@gmail.com>
2023-11-18 12:01:50 -0700

    whip up basic install script

---
+++ INSTALL.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# A quick and dirty way to set up these dotfiles in a fresh linux install
+# Configs are symlinked to this repo so they can be updated automatically
+
+echo \#\#\# ~ \#\#\#
+
+for filename in .*; do 
+    if [[ $filename == ".git" ]]
+    then
+        continue
+    fi
+
+    if [[ ! -L ~/$filename ]]
+    then
+        ln -s $filename ~/$filename
+    else
+        echo $filename already linked
+    fi
+done
+
+
+echo \#\#\# ~/.config \#\#\#
+
+cd dotconfig
+for filename in *; do
+    if [[ ! -L ~/.config/$filename ]]
+    then
+        ln -s ~/.config/$filename $filename
+    else
+        echo .config/$filename already linked 
+    fi 
+done
