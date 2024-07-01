0ee8c7954e98c5ea33df5a762f1947dde50d4f4d
Kleo Davidson <itskleonow@gmail.com>
2024-06-30 20:40:29 -0600

    feat: themes!

---
+++ dotconfig/i3/lock-navmesh.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#89b4facc'
+TEXT='#cba6f7bb'
+WRONG='#f38ba8bb'
+VERIFYING='#a6e3a1bb'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$VERIFYING         \
+--bshl-color=$WRONG          \
+\
+--screen 2                   \
+--blur 8                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"       \
